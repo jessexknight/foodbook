644e82832f46cbfc838bb99fa7b3f71b860b45d4
= <=>
2018-04-05 16:25:21 -0400

    first push

---
+++ index.php
@@ -0,0 +1,22 @@
+<html lang="en"><head>
+  <meta charset="UTF-8">
+  <title id='page-title'>Recipes</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link href='http://fonts.googleapis.com/css?family=Ubuntu&subset=cyrillic,latin' rel='stylesheet' type='text/css' />
+  <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+</head>
+<body>
+  <h1 id='title'></h1>
+
+  <h2>Time</h2>
+  <div id='time'></div>
+
+  <h2>Ingredients</h2>
+  <div id='ingredients'></div>
+
+  <h2>Instructions</h2>
+  <div id='instructions'></div>
+
+  <script src='js/load.js'></script>
+</body>
+</html>
---
+++ js/load.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+  function hoverover(str,match,hover){
+    return str;//.replace('???',' {'+match+'} ')
+  }
+  function checkbox(value=false){
+    return '<input type="checkbox" value='+value+'></input>'
+  }
+  function trow(str){
+    return '<tr>'+str+'</tr>';
+  }
+  function tcell(str,spec=''){
+    return '<td '+spec+'>'+str+'</td>';
+  }
+  $.getJSON('recipes/enchilada-casserole.json',function(data){
+    s = {}
+    // title
+    s['title'] = data['title']
+    s['page-title'] = data['title']
+    // time
+    s['time'] = '<table>'+
+      trow(tcell('Prep Time:')+tcell(data['time']['prep']))+
+      trow(tcell('Cook Time:')+tcell(data['time']['cook']))+
+      '</table>';
+    console.log(s['time'])
+    // ingredients
+    s['ingredients'] = '<table>'
+    for (var g in data['ingredients']){
+      s['ingredients'] += trow(
+        tcell(checkbox())+
+        tcell(data['ingredients'][g])+
+        tcell(g)
+      )
+    }
+    s['ingredients'] += '</table>'
+    // instructions
+    s['instructions'] = '<table>'
+    for (var i in data['instructions']){
+      instruction = data['instructions'][i]
+      for (var g in data['ingredients']){
+        instruction = hoverover(instruction,g.replace(/ *\([^)]*\) */g, ""),'')
+      }
+      s['instructions'] += trow(
+        tcell(checkbox(),   'valign="top"')+
+        tcell(parseInt(i)+1,'valign="top"')+
+        tcell(instruction)
+      )
+    }
+    s['instructions'] += '</table>'
+    // update the dom
+    $('#page-title').html(data['title'])
+    for (id in s){
+      $('#'+id).html(s[id])
+    };
+  });
+});
