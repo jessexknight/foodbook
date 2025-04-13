ec79659093c3bbfe09078ce783ec0d7f603611fd
jessexknight <jesse.x.knight@protonmail.com>
2025-04-13 16:21:33 +0100

    add description

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ for root,dirs,files in os.walk('food'):
   for file in files:
     if file == 'template.json': continue
     if root[-4:] == '.old': continue
+    print('> '+file)
     id = os.path.splitext(file)[0]
     with open(os.path.join(root,file),'r') as f:
       foods += [{'id':id,**json.load(f)}]
